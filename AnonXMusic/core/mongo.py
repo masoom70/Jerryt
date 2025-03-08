d9a6804c1e09b636198aba5db088432a9ae6e32a
NKMODNK6 <chotababbu654@gmail.com>
2025-03-08 12:03:35 +0530

    Update mongo.py
    Signed-off-by: NKMODNK6 <chotababbu654@gmail.com>

--- AnonXMusic/core/mongo.py
+++ AnonXMusic/core/mongo.py
@@ -1,14 +1,17 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-
-from config import MONGO_DB_URI
-
-from ..logging import LOGGER
-
-LOGGER(name).info("Connecting to your Mongo Database...")
-try:
-    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
-    mongodb = _mongo_async_.Anon
-    LOGGER(name).info("Connected to your Mongo Database.")
-except:
-    LOGGER(name).error("Failed to connect to your Mongo Database.")
-    exit()
+ 
+ from config import MONGO_DB_URI
+ 
+ from ..logging import LOGGER
+ 
+ LOGGER(name).info("Connecting to your Mongo Database...")
+ LOGGER(__name__).info("Connecting to your Mongo Database...")
+ try:
+     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
+     mongodb = _mongo_async_.Anon
+     LOGGER(name).info("Connected to your Mongo Database.")
+     LOGGER(__name__).info("Connected to your Mongo Database.")
+ except:
+     LOGGER(name).error("Failed to connect to your Mongo Database.")
+     LOGGER(__name__).error("Failed to connect to your Mongo Database.")
+     exit()
